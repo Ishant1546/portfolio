1b8dca2be31040f0cf2a0edef0fc780af02549c4
Ishant1546 <ishantchannel154@gmail.com>
2025-11-30 13:42:56 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MinecraftPortfolio from './minecraft-portfolio'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MinecraftPortfolio />
+    <App />
   </React.StrictMode>,
 )
